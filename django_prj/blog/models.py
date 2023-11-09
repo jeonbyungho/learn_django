f8acbd2ba1224a366a87f1b1826b52cc06d1c8f2
jeonbyungho <byho.jeon@gmail.com>
2023-11-09 12:52:36 +0900

    실습

--- django_prj/blog/models.py
+++ django_prj/blog/models.py
@@ -3,10 +3,16 @@ from django.db import models
 class Post(models.Model):
    # 제목
    title = models.CharField(max_length=50)
+
    # 내용
    content = models.TextField()
-   # 작성일
-   create_at = models.DateTimeField()
+
+   # 작성일 auto_now_add : 필드가 생성될 때 자동으로 값이 할당된다.
+   create_at = models.DateTimeField(auto_now_add=True)
+
+   # 수정 날짜 auto_now : 필드가 저장될 때 항상 현재 시간으로 자동으로 할당된다.
+   update_at = models.DateTimeField(auto_now=True)
+   
    # 작성자
    # author
 
