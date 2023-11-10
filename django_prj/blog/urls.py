3ef37509d330b52c9941b71e01c970fa411189e4
jeonbyungho <byho.jeon@gmail.com>
2023-11-10 11:30:21 +0900

    실습 - cbv detail 구현

--- django_prj/blog/urls.py
+++ django_prj/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path("", views.PostList.as_view(), name='blog'), # path("", views.index, name='blog'),
-   path("<int:post_pk>/", views.detail_post, name='detail'),
+   path("", views.PostList.as_view(), name='blog'),
+   path("<int:post_pk>/", views.PostDetail.as_view(), name='detail'),
 ]
--- django_prj/blog/views.py
+++ django_prj/blog/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
-
-# def index(request):
-#    posts = Post.objects.all().order_by('-pk')
-#    return render(request, "blog/index.html", {"posts": posts})
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
    model = Post
@@ -12,7 +8,8 @@ class PostList(ListView):
    context_object_name = "posts" # 
    ordering = ['-pk']
 
-
-def detail_post(request, post_pk):
-   post = Post.objects.get(pk=post_pk)
-   return render(request, "blog/detail.html", {"post": post})
+class PostDetail(DetailView):
+   model = Post
+   template_name = "blog/detail.html"
+   context_object_name = "post"
+   pk_url_kwarg = 'post_pk'
