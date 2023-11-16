1ea301a0df3cb02d196dbd90438c63d0188c63b8
jeonbyungho <byho.jeon@gmail.com>
2023-11-16 15:35:49 +0900

    실습 - 게시판 페이징

--- django_prj/blog/views.py
+++ django_prj/blog/views.py
@@ -14,12 +14,14 @@ class PostList(ListView):
    template_name = "blog/index.html" # 뷰 파일 지정
    context_object_name = "posts" # 
    ordering = ['-pk']
+   paginate_by = 4
 
    def get_context_data(self, **kwargs):
       # super 부모 클래스의 get_context_data()를 호출한다.
       context = super(PostList, self).get_context_data()
       context['categorys'] = Category.objects.all()
       context['no_category_post_count'] = Post.objects.filter(category=None).count()
+      
       return context
 
 # 게시판 목록 : 카테고리
